95399af6fed819a7c7b7ca3e7b4fe342fdc772b6
Rory <roryc89@googlemail.com>
2015-09-06 12:52:10 +0100

    extra modules added

---
+++ judgeVowelConsonantRatio.js
@@ -0,0 +1,15 @@
+
+var
+
+module.exports = function(board)
+{
+  for (var i = 0;  i < board.length; i++) {
+    var row = board[i];
+
+    for (var j = 0;  j < row.length -1; j++) {
+      var square = board[i][j];
+
+
+    }
+  }
+};
--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var createBoardWithContext = require('./createBoard');
 var Trie = require('./Trie');
 var rearrangeLetters = require('./rearrangeLetters');
 var solve = require('./solve');
+var fs = require('fs');
 
+var words = fs.readFileSync('/usr/share/dict/words', 'UTF-8').toUpperCase().split('\n');
 
 //dictionary trie
 var dict = new Trie();
@@ -15,11 +17,20 @@ for(var i = 0, len = prefix.length; i < len; i++)
   dict.insert(prefix[i]);
 }
 
+for(i = 0, len = words.length; i < len; i++)
+{
+  word = words[i];
+  if(word.length > 2)
+  {
+    dict.insert(word);
+  }
+}
+
+
 var start = process.hrtime();
 console.log("start");
 
 do{
-
   var result = solve(boardContext, dict);
 
   if(result)
